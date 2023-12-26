add30259c3ba4a11f548d223a9ae31669d064e1f
bhuvavivek <vivekpate26@gmail.com>
2023-12-26 12:17:20 +0530

    card details api

--- src/pages/ManageFootage/index.jsx
+++ src/pages/ManageFootage/index.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import { Button, Img, Input, Text } from "components";
 
 import Sidebar1 from "components/Sidebar1";
 import { Link } from "react-router-dom";
+import { api } from "utils/api";
 import { CloseSVG } from "../../assets/images";
 
 const ManageFootagePage = () => {
@@ -15,6 +16,47 @@ const ManageFootagePage = () => {
 
   // Example: Get the value of the 'footageid' query parameter
   const footageId = queryParams.get("footageid");
+
+  const [footageDetails, setFootageDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchFootageDetails = async () => {
+      try {
+        const response = await api.get(`/admin/footage/details/${footageId}`);
+        setFootageDetails(response.data.result);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    if (footageId) {
+      fetchFootageDetails();
+    }
+  }, [footageId]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  // Assuming footageDetails has the structure of the API response
+  const {
+    name,
+    price,
+    id,
+    description,
+
+    date,
+    time,
+    thumbnail,
+  } = footageDetails;
 
   return (
     <>
@@ -67,17 +109,17 @@ const ManageFootagePage = () => {
                 <div className="flex md:flex-1 flex-col md:gap-10 gap-10 justify-start md:mt-0 mt-2 w-[43%] md:w-full">
                   <Img
                     className="h-[331px] sm:h-auto object-cover w-full"
-                    src="images/img_rectangle39_331x407.png"
-                    alt="rectangleThirtyNine"
+                    src={thumbnail}
+                    alt="Footage Thumbnail"
                   />
                 </div>
                 {/* carderails-right end */}
                 {/* cardetails-left start  */}
-                <div className="flex md:flex-1 flex-col font-sourcesanspro gap-6 items-start justify-start w-auto md:w-full">
+                <div className="flex md:flex-1 flex-col font-sourcesanspro gap-6 items-start justify-start w-full">
                   <div className="flex flex-col items-start justify-start w-[55%] md:w-full">
                     <div className="flex flex-col items-center justify-start w-auto sm:w-full">
                       <Text className="md:text-3xl sm:text-[28px] text-3xl font-semibold text-blue_gray-900_01 w-auto source-sans">
-                        Name: Car crash surveillance
+                        Name:{name}
                       </Text>
                     </div>
                   </div>
@@ -88,14 +130,14 @@ const ManageFootagePage = () => {
                           ID:
                         </Text>
                         <Text className="text-blue_gray-900_01 text-lg font-normal font-lato">
-                          #1350678
+                          {id}
                         </Text>
                       </div>
                     </div>
                   </div>
                   <div className="flex flex-col font-sourcesanspro items-center justify-start">
                     <Text className="md:text-3xl sm:text-[28px] text-3xl font-semibold text-blue_gray-900_01 w-auto source-sans">
-                      Price: $25.00
+                      Price:${price}
                     </Text>
                   </div>
                   <div className="flex flex-col font-lato items-center justify-start w-[57%] md:w-full">
@@ -108,7 +150,7 @@ const ManageFootagePage = () => {
                           className="text-blue_gray-900_01 text-lg"
                           size="txtLatoRegular18"
                         >
-                          12/08/2023
+                          {date}
                         </Text>
                       </div>
                       <div className="flex flex-row items-start justify-start gap-1 w-[53%]">
@@ -119,12 +161,12 @@ const ManageFootagePage = () => {
                           className=" text-blue_gray-900_01 text-lg text-left"
                           size="txtLatoRegular18"
                         >
-                          10:00 pm GMT+1
+                          {time}
                         </Text>
                       </div>
                     </div>
                   </div>
-                  <div className="flex flex-col font-lato items-center justify-start w-full">
+                  <div className="flex flex-col font-lato items-start justify-start w-full">
                     <div className="flex flex-col gap-[7px] items-start justify-start w-full">
                       <Text className="ml-0.5 md:ml-[0] text-center text-gray-900 text-lg font-bold leading-6">
                         Description:
@@ -133,11 +175,7 @@ const ManageFootagePage = () => {
                         className="leading-6 text-blue_gray-900_01 text-lg w-[90%]"
                         size="txtLatoRegular18"
                       >
-                        Lorem ipsum dolor sit amet consectetur. Pretium pretium
-                        nisl pulvinar in in sed sit. Viverra ut morbi feugiat
-                        dolor aliquam diam. Consectetur elementum eget neque
-                        urna sed viverra. Turpis risus in non eget aliquam
-                        tincidunt pharetra.
+                        {description}
                       </Text>
                     </div>
                   </div>
