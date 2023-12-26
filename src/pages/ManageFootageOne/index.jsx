329f06b779ec1638fbaa0e2ab079c554152fcdb3
bhuvavivek <vivekpate26@gmail.com>
2023-12-26 11:33:13 +0530

    the manage footage

--- src/pages/ManageFootageOne/index.jsx
+++ src/pages/ManageFootageOne/index.jsx
@@ -1,21 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Button, Img, Input, List, SelectBox, Text } from "components";
 
+import { Cities, States } from "assets/state-city";
 import Sidebar1 from "components/Sidebar1";
 import { Link } from "react-router-dom";
+import { api } from "utils/api";
 import { CloseSVG } from "../../assets/images";
 
-const buttonOptionsList = [
-  { label: "Option1", value: "option1" },
-  { label: "Option2", value: "option2" },
-  { label: "Option3", value: "option3" },
-];
-const buttonOneOptionsList = [
-  { label: "Option1", value: "option1" },
-  { label: "Option2", value: "option2" },
-  { label: "Option3", value: "option3" },
-];
 const buttonTwoOptionsList = [
   { label: "Option1", value: "option1" },
   { label: "Option2", value: "option2" },
@@ -24,6 +16,34 @@ const buttonTwoOptionsList = [
 
 const ManageFootageOnePage = () => {
   const [frame348value, setFrame348value] = React.useState("");
+
+  const [state, setState] = useState();
+  const [city, setCity] = useState();
+
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  const fetchData = async () => {
+    try {
+      const response = await api.get(
+        `/admin/footage?state=West Bengal&city=Kolkata&fromDate=2024-01-01&toDate=2024-12-31&name=car&sortBy=new&page=${page}&limit=15`
+      );
+
+      setData(response.data.result);
+      setTotalPages(Math.ceil(response.data.totalCount / 15));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
 
   return (
     <>
@@ -100,13 +120,18 @@ const ManageFootageOnePage = () => {
                       }
                       isMulti={false}
                       name="button"
-                      options={buttonOptionsList}
+                      options={States}
                       isSearchable={false}
                       placeholder="Select state"
                       shape="round"
                       color="white_A700"
                       size="xs"
                       variant="fill"
+                      value={state}
+                      onChange={(selectedOption) => {
+                        setState(selectedOption);
+                        setCity(null);
+                      }}
                     />
                     <SelectBox
                       className="border border-gray-500_7f border-solid font-semibold text-left text-sm w-[28%] md:w-full"
@@ -120,12 +145,16 @@ const ManageFootageOnePage = () => {
                       }
                       isMulti={false}
                       name="button_One"
-                      options={buttonOneOptionsList}
+                      options={Cities[state?.replace(/\s/g, "")]}
                       isSearchable={false}
                       placeholder="Select City"
                       shape="round"
                       color="white_A700"
                       size="xs"
+                      value={city}
+                      onChange={(optionvalue) => {
+                        setCity(optionvalue);
+                      }}
                       variant="fill"
                     />
                     <SelectBox
@@ -159,12 +188,12 @@ const ManageFootageOnePage = () => {
                 >
                   <div className="flex flex-1 flex-col gap-2 items-center justify-start w-full ">
                     <div className="gap-10 grid sm:grid-cols-1 md:grid-cols-3 grid-cols-5 items-center justify-between w-full">
-                      {Array.from({ length: 10 }).map((item, i) => (
+                      {data.map((item, i) => (
                         <Link to={"/managefootage"} key={i}>
                           <Img
                             className="common-pointer flex-1 h-[157px] md:h-auto object-cover rounded-[16px] w-full"
-                            src="images/img_rectangle39.png"
-                            alt="rectangleThirtyNine"
+                            src={item.thumbnail}
+                            alt={`Thumbnail for ${item.name}`}
                           />
                           <Text
                             className="leading-[24.00px] text-base text-gray-900_01 w-full mt-2"
@@ -172,12 +201,12 @@ const ManageFootageOnePage = () => {
                           >
                             <span className="text-gray-900_01 font-lato text-left font-normal">
                               <>
-                                Car crash surveillance
+                                {item.name}
                                 <br />
                               </>
                             </span>
                             <span className="text-blue-700 font-lato text-left font-medium">
-                              $25.00
+                              ${item.price}
                             </span>
                             <span className="text-gray-900_01 font-lato text-left font-medium">
                               <span>
@@ -186,7 +215,7 @@ const ManageFootageOnePage = () => {
                               </span>
                             </span>
                             <span className="text-gray-900_01 font-lato text-left text-sm font-medium">
-                              12/08/23
+                              {item.date} {item.time}
                             </span>
                             <span className="text-gray-900_01 font-lato text-left font-medium">
                               <>
@@ -205,15 +234,27 @@ const ManageFootageOnePage = () => {
             </div>
             <div className="flex flex-col items-center justify-start md:ml-[0] ml-[53%] w-[43%] md:w-full mb-10">
               <div className="flex sm:flex-col gap-10 flex-row sm:gap-14 items-center justify-between w-full">
-                <div className="bg-white-A700 border-2 border-indigo-900 border-solid  flex flex-col items-center justify-start p-3 rounded-[16px] w-[30%] sm:w-full">
+                <Button
+                  onClick={() => {
+                    if (page > 1) {
+                      setPage(page - 1);
+                    }
+                  }}
+                  className="bg-white-A700 border-2 border-indigo-900 border-solid  flex flex-col items-center justify-start p-3 rounded-[16px] w-[30%] sm:w-full"
+                >
                   <Img
                     className="h-6 w-6"
                     src="images/img_arrowdown.svg"
                     alt="arrowdown"
                   />
-                </div>
+                </Button>
                 <Button
                   className="cursor-pointer flex items-center justify-center min-w-[150px]"
+                  onClick={() => {
+                    if (page < totalPages) {
+                      setPage(page + 1);
+                    }
+                  }}
                   rightIcon={
                     <Img
                       className="h-6 ml-[5px]"
@@ -239,10 +280,10 @@ const ManageFootageOnePage = () => {
                     size="sm"
                     variant="fill"
                   >
-                    1
+                    {page}
                   </Button>
                   <Text className="text-base text-center font-semibold text-[#212121] -ml-5 ">
-                    <pre>of 300</pre>
+                    <pre>of {totalPages}</pre>
                   </Text>
                 </div>
               </div>
